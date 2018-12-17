79dbb67a7725833baf06bc4b31c4bbc9baa13980
Sahin Caliskan <sahinc@google.com>
2018-12-17 13:10:27 -0800

    RCS thread and participant implementations [provider]
    This change makes it so that threads and participants APIs now work. It
    is now possible to query, insert, update and delete threads and
    participants, as well as their fields.
    
    Test: Added unit tests
    
    Bug: 109759350
    Change-Id: I27236d330ea68b3c0c9e055e22c7d85edef3a2a9

--- src/com/android/providers/telephony/RcsProvider.java
+++ src/com/android/providers/telephony/RcsProvider.java
@@ -20,35 +20,82 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.util.Log;
 
+import com.android.internal.annotations.VisibleForTesting;
+
 /**
  * Content provider to handle RCS messages. The functionality here is similar to SmsProvider,
- * MmsProvider etc. This is not meant to be public.
+ * MmsProvider etc.
  *
  * @hide
  */
 public class RcsProvider extends ContentProvider {
     static final String TAG = "RcsProvider";
     static final String AUTHORITY = "rcs";
+    static final long TRANSACTION_FAILED = Long.MIN_VALUE;
 
     private static final Uri THREAD_URI_PREFIX = Uri.parse("content://" + AUTHORITY + "/thread");
     private static final Uri PARTICIPANT_URI_PREFIX = Uri.parse(
             "content://" + AUTHORITY + "/participant");
+    private static final Uri P2P_THREAD_URI_PREFIX = Uri.parse(
+            "content://" + AUTHORITY + "/p2p_thread");
+    private static final Uri GROUP_THREAD_URI_PREFIX = Uri.parse(
+            "content://" + AUTHORITY + "/group_thread");
 
     private static final UriMatcher URL_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
-    private static final int THREAD = 1;
-    private static final int PARTICIPANT = 2;
+
+    private static final int UNIFIED_RCS_THREAD = 1;
+    private static final int UNIFIED_RCS_THREAD_WITH_ID = 2;
+    private static final int PARTICIPANT = 3;
+    private static final int PARTICIPANT_WITH_ID = 4;
+    private static final int P2P_THREAD = 5;
+    private static final int P2P_THREAD_WITH_ID = 6;
+    private static final int P2P_THREAD_PARTICIPANT = 7;
+    private static final int GROUP_THREAD = 8;
+    private static final int GROUP_THREAD_WITH_ID = 9;
+    private static final int GROUP_THREAD_PARTICIPANT = 10;
 
     SQLiteOpenHelper mDbOpenHelper;
 
+    @VisibleForTesting
+    RcsProviderThreadHelper mThreadHelper;
+    @VisibleForTesting
+    RcsProviderParticipantHelper mParticipantHelper;
+
     static {
-        URL_MATCHER.addURI(AUTHORITY, "thread", THREAD);
+        // example query: content://rcs/thread?owner_participant=3
+        URL_MATCHER.addURI(AUTHORITY, "thread", UNIFIED_RCS_THREAD);
+
+        // example query: content://rcs/thread/4
+        URL_MATCHER.addURI(AUTHORITY, "thread/#", UNIFIED_RCS_THREAD_WITH_ID);
+
+        // example query: content://rcs/participant?alias="alice"
         URL_MATCHER.addURI(AUTHORITY, "participant", PARTICIPANT);
+
+        // example query: content://rcs/participant/12
+        URL_MATCHER.addURI(AUTHORITY, "participant/#", PARTICIPANT_WITH_ID);
+
+        // example query: content://rcs/p2p_thread?rcs_fallback_thread_id=6
+        URL_MATCHER.addURI(AUTHORITY, "p2p_thread", P2P_THREAD);
+
+        // example query: content://rcs/p2p_thread/4 , where 4 is the _id in rcs_threads table.
+        URL_MATCHER.addURI(AUTHORITY, "p2p_thread/#", P2P_THREAD_WITH_ID);
+
+        // example query: content://rcs/p2p_thread/7/participant?alias="bob"
+        URL_MATCHER.addURI(AUTHORITY, "p2p_thread/#/participant", P2P_THREAD_PARTICIPANT);
+
+        // example query: content://rcs/group_thread?group_name="best friends"
+        URL_MATCHER.addURI(AUTHORITY, "group_thread", GROUP_THREAD);
+
+        // example query: content://rcs/group_thread/13, where 13 is the _id in rcs_threads table.
+        URL_MATCHER.addURI(AUTHORITY, "group_thread/#", GROUP_THREAD_WITH_ID);
+
+        // example query: content://rcs/group_thread/18/participant?alias="charlie"
+        URL_MATCHER.addURI(AUTHORITY, "group_thread/#/participant",
+                GROUP_THREAD_PARTICIPANT);
     }
 
     @Override
@@ -56,29 +103,48 @@ public class RcsProvider extends ContentProvider {
         setAppOps(AppOpsManager.OP_READ_SMS, AppOpsManager.OP_WRITE_SMS);
         // Use the credential encrypted mmssms.db for RCS messages.
         mDbOpenHelper = MmsSmsDatabaseHelper.getInstanceForCe(getContext());
+        mParticipantHelper = new RcsProviderParticipantHelper(mDbOpenHelper);
+        mThreadHelper = new RcsProviderThreadHelper(mDbOpenHelper);
         return true;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-            String sortOrder) {
+    public synchronized Cursor query(Uri uri, String[] projection, String selection,
+            String[] selectionArgs, String sortOrder) {
         int match = URL_MATCHER.match(uri);
-        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
-        SQLiteDatabase readableDatabase = mDbOpenHelper.getReadableDatabase();
 
         switch (match) {
-            case THREAD:
-                RcsProviderThreadHelper.buildThreadQuery(qb);
-                break;
+            case UNIFIED_RCS_THREAD:
+                return mThreadHelper.queryUnifiedThread(projection, selection,
+                        selectionArgs, sortOrder);
+            case UNIFIED_RCS_THREAD_WITH_ID:
+                return mThreadHelper.queryUnifiedThreadUsingId(uri, projection);
             case PARTICIPANT:
-                RcsProviderParticipantHelper.buildParticipantsQuery(qb);
+                return mParticipantHelper.queryParticipant(projection, selection,
+                        selectionArgs, sortOrder);
+            case PARTICIPANT_WITH_ID:
+                return mParticipantHelper.queryParticipantWithId(uri, projection);
+            case P2P_THREAD:
+                return mThreadHelper.query1to1Thread(projection, selection,
+                        selectionArgs, sortOrder);
+            case P2P_THREAD_WITH_ID:
+                return mThreadHelper.query1To1ThreadUsingId(uri, projection);
+            case P2P_THREAD_PARTICIPANT:
+                // TODO(109759350) - implement
+                break;
+            case GROUP_THREAD:
+                return mThreadHelper.queryGroupThread(projection, selection,
+                        selectionArgs, sortOrder);
+            case GROUP_THREAD_WITH_ID:
+                return mThreadHelper.queryGroupThreadUsingId(uri, projection);
+            case GROUP_THREAD_PARTICIPANT:
+                // TODO(109759350) - implement
                 break;
             default:
                 Log.e(TAG, "Invalid query: " + uri);
         }
 
-        return qb.query(
-                readableDatabase, projection, selection, selectionArgs, null, null, sortOrder);
+        return null;
     }
 
     @Override
@@ -87,85 +153,133 @@ public class RcsProvider extends ContentProvider {
     }
 
     @Override
-    public Uri insert(Uri uri, ContentValues values) {
+    public synchronized Uri insert(Uri uri, ContentValues values) {
         int match = URL_MATCHER.match(uri);
-        SQLiteDatabase writableDatabase = mDbOpenHelper.getWritableDatabase();
-        writableDatabase.beginTransaction();
-
         Uri returnUri = null;
         long rowId;
 
-        try {
-            switch (match) {
-                case THREAD:
-                    rowId = RcsProviderThreadHelper.insert(writableDatabase, values);
-                    returnUri = THREAD_URI_PREFIX.buildUpon().appendPath(
-                            Long.toString(rowId)).build();
-                    break;
-                case PARTICIPANT:
-                    rowId = RcsProviderParticipantHelper.insert(writableDatabase, values);
-                    returnUri = PARTICIPANT_URI_PREFIX.buildUpon().appendPath(
-                            Long.toString(rowId)).build();
-                    break;
-                default:
-                    Log.e(TAG, "Invalid insert: " + uri);
-            }
-        } finally {
-            writableDatabase.endTransaction();
+        switch (match) {
+            case UNIFIED_RCS_THREAD:
+                Log.e(TAG, "Inserting into unified thread view is not supported, uri:" + uri);
+                break;
+            case UNIFIED_RCS_THREAD_WITH_ID:
+                Log.e(TAG, "Inserting a thread with a specified ID is not supported, uri:" + uri);
+                break;
+            case PARTICIPANT:
+                rowId = mParticipantHelper.insertParticipant(values);
+                if (rowId == TRANSACTION_FAILED) {
+                    return null;
+                }
+                returnUri = PARTICIPANT_URI_PREFIX.buildUpon().appendPath(
+                        Long.toString(rowId)).build();
+                break;
+            case PARTICIPANT_WITH_ID:
+                Log.e(TAG, "Inserting participant with a specified ID is not supported, uri:"
+                        + uri);
+                break;
+            case P2P_THREAD:
+                rowId = mThreadHelper.insert1To1Thread(values);
+                if (rowId == TRANSACTION_FAILED) {
+                    return null;
+                }
+                returnUri = P2P_THREAD_URI_PREFIX.buildUpon().appendPath(
+                        Long.toString(rowId)).build();
+                break;
+            case P2P_THREAD_WITH_ID:
+                Log.e(TAG, "Inserting a thread with a specified ID is not supported, uri:" + uri);
+                break;
+            case P2P_THREAD_PARTICIPANT:
+                // TODO(109759350) - implement
+                break;
+            case GROUP_THREAD:
+                rowId = mThreadHelper.insertGroupThread(values);
+                if (rowId == TRANSACTION_FAILED) {
+                    return null;
+                }
+                returnUri = GROUP_THREAD_URI_PREFIX.buildUpon().appendPath(
+                        Long.toString(rowId)).build();
+                break;
+            case GROUP_THREAD_WITH_ID:
+                Log.e(TAG, "Inserting a thread with a specified ID is not supported, uri:" + uri);
+                break;
+            case GROUP_THREAD_PARTICIPANT:
+                // TODO(109759350) - implement
+                break;
+            default:
+                Log.e(TAG, "Invalid insert: " + uri);
         }
 
         return returnUri;
     }
 
     @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
+    public synchronized int delete(Uri uri, String selection, String[] selectionArgs) {
         int match = URL_MATCHER.match(uri);
         int deletedCount = 0;
-        SQLiteDatabase writableDatabase = mDbOpenHelper.getWritableDatabase();
-        writableDatabase.beginTransaction();
 
-        try {
-            switch (match) {
-                case THREAD:
-                    deletedCount = RcsProviderThreadHelper.delete(writableDatabase, selection,
-                            selectionArgs);
-                    break;
-                case PARTICIPANT:
-                    deletedCount = RcsProviderParticipantHelper.delete(writableDatabase, selection,
-                            selectionArgs);
-                    break;
-                default:
-                    Log.e(TAG, "Invalid delete: " + uri);
-            }
-        } finally {
-            writableDatabase.endTransaction();
+        switch (match) {
+            case UNIFIED_RCS_THREAD:
+                return mThreadHelper.deleteUnifiedThread(selection, selectionArgs);
+            case UNIFIED_RCS_THREAD_WITH_ID:
+                return mThreadHelper.deleteUnifiedThreadWithId(uri);
+            case PARTICIPANT:
+                return mParticipantHelper.deleteParticipant(selection, selectionArgs);
+            case PARTICIPANT_WITH_ID:
+                return mParticipantHelper.deleteParticipantWithId(uri);
+            case P2P_THREAD:
+                return mThreadHelper.delete1To1Thread(selection, selectionArgs);
+            case P2P_THREAD_WITH_ID:
+                return mThreadHelper.delete1To1ThreadWithId(uri);
+            case P2P_THREAD_PARTICIPANT:
+                // TODO(109759350) - implement
+                break;
+            case GROUP_THREAD:
+                return mThreadHelper.deleteGroupThread(selection, selectionArgs);
+            case GROUP_THREAD_WITH_ID:
+                return mThreadHelper.deleteGroupThreadWithId(uri);
+            case GROUP_THREAD_PARTICIPANT:
+                // TODO(109759350) - implement
+                break;
+            default:
+                Log.e(TAG, "Invalid delete: " + uri);
         }
 
         return deletedCount;
     }
 
     @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+    public synchronized int update(Uri uri, ContentValues values, String selection,
+            String[] selectionArgs) {
         int match = URL_MATCHER.match(uri);
         int updatedCount = 0;
-        SQLiteDatabase writableDatabase = mDbOpenHelper.getWritableDatabase();
 
-        writableDatabase.beginTransaction();
-        try {
-            switch (match) {
-                case THREAD:
-                    updatedCount = RcsProviderThreadHelper.update(
-                            writableDatabase, values, selection, selectionArgs);
-                    break;
-                case PARTICIPANT:
-                    updatedCount = RcsProviderParticipantHelper.update(
-                            writableDatabase, values, selection, selectionArgs);
-                    break;
-                default:
-                    Log.e(TAG, "Invalid update: " + uri);
-            }
-        } finally {
-            writableDatabase.endTransaction();
+        switch (match) {
+            case UNIFIED_RCS_THREAD:
+                Log.e(TAG, "Updating unified thread view is not supported, uri: " + uri);
+                break;
+            case UNIFIED_RCS_THREAD_WITH_ID:
+                Log.e(TAG, "Updating unified thread view is not supported, uri: " + uri);
+                break;
+            case PARTICIPANT:
+                return mParticipantHelper.updateParticipant(values, selection, selectionArgs);
+            case PARTICIPANT_WITH_ID:
+                return mParticipantHelper.updateParticipantWithId(values, uri);
+            case P2P_THREAD:
+                return mThreadHelper.update1To1Thread(values, selection, selectionArgs);
+            case P2P_THREAD_WITH_ID:
+                return mThreadHelper.update1To1ThreadWithId(values, uri);
+            case P2P_THREAD_PARTICIPANT:
+                Log.e(TAG, "Updating junction table entries is supported, uri: " + uri);
+                break;
+            case GROUP_THREAD:
+                return mThreadHelper.updateGroupThread(values, selection, selectionArgs);
+            case GROUP_THREAD_WITH_ID:
+                return mThreadHelper.updateGroupThreadWithId(values, uri);
+            case GROUP_THREAD_PARTICIPANT:
+                Log.e(TAG, "Updating junction table entries is supported, uri: " + uri);
+                break;
+            default:
+                Log.e(TAG, "Invalid update: " + uri);
         }
         return updatedCount;
     }
---
+++ tests/src/com/android/providers/telephony/RcsProviderDeleteTest.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.android.providers.telephony;
+
+import static com.android.providers.telephony.RcsProviderParticipantHelper.CANONICAL_ADDRESS_ID_COLUMN;
+import static com.android.providers.telephony.RcsProviderParticipantHelper.RCS_ALIAS_COLUMN;
+import static com.android.providers.telephony.RcsProviderThreadHelper.FALLBACK_THREAD_ID_COLUMN;
+import static com.android.providers.telephony.RcsProviderThreadHelper.GROUP_NAME_COLUMN;
+import static com.android.providers.telephony.RcsProviderThreadHelper.OWNER_PARTICIPANT;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.mock.MockContentResolver;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(AndroidJUnit4.class)
+public class RcsProviderDeleteTest {
+    private MockContentResolver mContentResolver;
+    private RcsProviderTestable mRcsProvider;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mRcsProvider = new RcsProviderTestable();
+        RcsProviderTestable.MockContextWithProvider
+                context = new RcsProviderTestable.MockContextWithProvider(mRcsProvider);
+        mContentResolver = context.getContentResolver();
+
+        // insert a participant
+        //  first into the MmsSmsProvider
+        mRcsProvider.getWritableDatabase().execSQL(
+                "INSERT INTO canonical_addresses VALUES (1, \"+15551234567\")");
+
+        //  then into the RcsProvider
+        ContentValues participantValues = new ContentValues();
+        participantValues.put(RCS_ALIAS_COLUMN, "Bob");
+        participantValues.put(CANONICAL_ADDRESS_ID_COLUMN, 1);
+        assertThat(mContentResolver.insert(Uri.parse("content://rcs/participant"),
+                participantValues)).isEqualTo(Uri.parse("content://rcs/participant/1"));
+
+        // insert one 1 to 1 thread
+        ContentValues p2pContentValues = new ContentValues();
+        Uri p2pThreadUri = Uri.parse("content://rcs/p2p_thread");
+        p2pContentValues.put(FALLBACK_THREAD_ID_COLUMN, 1);
+        assertThat(mContentResolver.insert(p2pThreadUri, p2pContentValues)).isEqualTo(
+                Uri.parse("content://rcs/p2p_thread/1"));
+
+        // insert one group thread
+        ContentValues groupContentValues = new ContentValues();
+        groupContentValues.put(OWNER_PARTICIPANT, 1);
+        groupContentValues.put(GROUP_NAME_COLUMN, "name");
+        Uri groupThreadUri = Uri.parse("content://rcs/group_thread");
+        assertThat(mContentResolver.insert(groupThreadUri, groupContentValues)).isEqualTo(
+                Uri.parse("content://rcs/group_thread/2"));
+    }
+
+    @After
+    public void tearDown() {
+        mRcsProvider.tearDown();
+    }
+
+    @Test
+    public void testDeleteThreadWithId() {
+        assertThat(
+                mContentResolver.delete(Uri.parse("content://rcs/thread/1"), null, null)).isEqualTo(
+                1);
+        assertDeletionViaQuery("content://rcs/thread/1");
+    }
+
+    @Test
+    public void testDeleteThreadWithSelection() {
+        assertThat(mContentResolver.delete(Uri.parse("content://rcs/thread"), "owner_participant=1",
+                null)).isEqualTo(1);
+        assertDeletionViaQuery("content://rcs/thread/2");
+    }
+
+    @Test
+    public void testDelete1To1ThreadWithId() {
+        assertThat(mContentResolver.delete(Uri.parse("content://rcs/p2p_thread/1"), null,
+                null)).isEqualTo(1);
+        assertDeletionViaQuery("content://rcs/p2p_thread/1");
+    }
+
+    @Test
+    public void testDelete1To1ThreadWithSelection() {
+        assertThat(mContentResolver.delete(Uri.parse("content://rcs/p2p_thread"),
+                "rcs_fallback_thread_id=1", null)).isEqualTo(1);
+        assertDeletionViaQuery("content://rcs/p2p_thread/1");
+    }
+
+    @Test
+    public void testDeleteGroupThreadWithId() {
+        assertThat(mContentResolver.delete(Uri.parse("content://rcs/group_thread/2"), null,
+                null)).isEqualTo(1);
+        assertDeletionViaQuery("content://rcs/group_thread/2");
+    }
+
+    @Test
+    public void testDeleteGroupThreadWithSelection() {
+        assertThat(mContentResolver.delete(Uri.parse("content://rcs/group_thread"),
+                "group_name=\"name\"", null)).isEqualTo(1);
+        assertDeletionViaQuery("content://rcs/group_thread/1");
+    }
+
+    @Test
+    public void testDeleteParticipantWithId() {
+        assertThat(mContentResolver.delete(Uri.parse("content://rcs/participant/1"), null,
+                null)).isEqualTo(1);
+        assertDeletionViaQuery("content://rcs/participant/1");
+    }
+
+    @Test
+    public void testDeleteParticipantWithSelection() {
+        assertThat(
+                mContentResolver.delete(Uri.parse("content://rcs/participant"), "rcs_alias=\"Bob\"",
+                        null)).isEqualTo(1);
+        assertDeletionViaQuery("content://rcs/participant/1");
+    }
+
+    private void assertDeletionViaQuery(String queryUri) {
+        Cursor cursor = mContentResolver.query(Uri.parse(queryUri), null, null, null);
+        assertThat(cursor.getCount()).isEqualTo(0);
+        cursor.close();
+    }
+}
